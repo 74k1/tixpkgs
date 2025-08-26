b223683bf98054e84fab8e158e81d7fb8cbc0a44
74k1 <git.t@betsumei.com>
2025-08-26 18:55:45 +0200

    fix(outerbase-studio-desktop): add .desktop file & icon

--- pkgs/ou/outerbase-studio-desktop/default.nix
+++ pkgs/ou/outerbase-studio-desktop/default.nix
@@ -3,8 +3,7 @@
   stdenv,
   fetchurl,
   appimageTools,
-}:
-let
+}: let
   pname = "outerbase-studio-desktop";
   version = "0.1.29";
 
@@ -17,17 +16,25 @@ in
 appimageTools.wrapType2 rec {
   inherit pname version src;
 
-  extraInstallCommands = /* sh */ ''
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace-fail 'Exec=AppRun' 'Exec=${meta.mainProgram}'
-  '';
+  extraInstallCommands = let
+    contents = appimageTools.extractType2 {inherit pname version src;};
+  in
+    /*
+    sh
+    */
+    ''
+      mkdir -p "$out/share/applications"
+      install -m 444 ${contents}/${meta.mainProgram}.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/${pname}.desktop --replace-fail 'Exec=AppRun' 'Exec=${meta.mainProgram}'
+      cp -r ${contents}/usr/share/icons $out/share
+    '';
 
   meta = with lib; {
     description = "A lightweight Electron wrapper for the Outerbase Studio web version.";
     homepage = "https://www.outerbase.com/";
     license = licenses.agpl3Plus;
     maintainers = [maintainers."74k1"];
-    mainProgram = "Outerbase-Studio-Desktop";
+    mainProgram = "outerbase-studio-desktop";
     platforms = ["x86_64-linux"];
   };
 }
