3f15d4f24c62f49a51970dc5cfdb407ef5bc13f8
74k1 <git.t@betsumei.com>
2025-09-10 23:42:37 +0200

    feat(mtkclient): init

---
+++ pkgs/mt/mtkclient/default.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  lib,
+  python3,
+  fetchFromGitHub,
+}:
+python3.pkgs.buildPythonPackage rec {
+  pyproject = true;
+  pname = "mtkclient";
+  version = "031957d";
+
+  buildInputs = with pkgs; [
+    pkgs.keystone
+  ];
+
+  propagatedBuildInputs = with python3.pkgs; [
+    hatchling
+    capstone
+    colorama
+    flake8
+    fusepy
+    keystone-engine
+    pycryptodome
+    pycryptodomex
+    pyserial
+    pyside6
+    pyusb
+    setuptools
+    shiboken6
+    unicorn
+  ];
+
+  src = fetchFromGitHub {
+    owner = "bkerler";
+    repo = "mtkclient";
+    rev = "031957d948e1f6ac795c4d1572c37a15bcde192b";
+    hash = "sha256-Yx4/ZZDsXoAorysGpO2913+aq6OMwm7ITASGchOvD60=";
+  };
+
+  postFixup = ''
+    mkdir -p $out/opt/mtkclient
+    mv * $out/opt/mtkclient
+
+    rm -rf $out/opt/mtkclient/mtkclient/Windows
+
+    mkdir -p $out/lib/udev/rules.d
+    cp $out/opt/mtkclient/mtkclient/Setup/Linux/51-edl.rules $out/lib/udev/rules.d/52-edl.rules
+  '';
+
+  meta = {
+    description = "MTK reverse engineering and flash tool";
+    homepage = "https://github.com/bkerler/mtkclient";
+    license = lib.licenses.gpl3Only;
+  };
+}
