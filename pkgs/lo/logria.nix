0c2f4c42ac752acbb44670c549f8fd9b83675ada
74k1 <git.t@betsumei.com>
2025-11-25 23:13:25 +0100

    feat(logria): 0.2.0 -> 0.4.2

--- pkgs/lo/logria.nix
+++ pkgs/lo/logria.nix
@@ -5,17 +5,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "logria";
-  version = "0.2.0";
+  version = "0.4.2";
 
   src = fetchFromGitHub {
     owner = "ReagentX";
     repo = "logria";
     rev = "${version}";
-    hash = "sha256-09U/M527X78IOOzKYhRdep0Ql8ec4Q8VMkeA5iwuqtQ=";
+    hash = "sha256-5tq+kD0jiaAtC7YYD0H4gYK95+XWCaay7vo5z5WBhU8=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-cdZQEpnqDasB4TP2pzsfDHk9KUYIPgtXnR6N6hIVy+Y=";
+  cargoHash = "sha256-nKrMzwTOW8MeOY16w3lSNadQa+mf8GHpQiwyLCJ5uLU=";
 
   doCheck = false;
 
