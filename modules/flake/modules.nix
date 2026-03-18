bb4e139d322abdf9ac9eaacf6059ff7737fcc2d5
74k1 <git.t@betsumei.com>
2026-03-18 14:40:54 +0100

    feat(home-manager): add support for home-manager modules

--- modules/flake/modules.nix
+++ modules/flake/modules.nix
@@ -4,52 +4,47 @@
   ...
 }: {
   flake = let
-    modules-base = "${inputs.self.outPath}/modules/nixos";
+    mkImportedModules = modulesBase:
+      let
+        nixFiles = lib.filesystem.listFilesRecursive modulesBase;
 
-    # Get all .nix files in ./by-name directories
-    nixFiles = lib.filesystem.listFilesRecursive modules-base;
+        filteredFiles = builtins.filter (file: lib.strings.hasSuffix ".nix" file) nixFiles;
 
-    # Filter to only keep files that match the pattern */${name}.nix
-    filteredFiles =
-      builtins.filter
-      (file: lib.strings.hasSuffix ".nix" file)
-      nixFiles;
+        instantiateModule = importedModule: {pkgs, ...}: let
+          functionArgs = builtins.functionArgs importedModule;
+          extraArgs =
+            lib.optionalAttrs (builtins.hasAttr "inputs" functionArgs) {
+              inherit inputs;
+            }
+            // lib.optionalAttrs (builtins.hasAttr "tixpkgs" functionArgs) {
+              tixpkgs = inputs.self.packages.${pkgs.stdenv.hostPlatform.system};
+            };
+        in {
+          imports = [
+            (if extraArgs != { } then importedModule extraArgs else importedModule)
+          ];
+        };
+      in
+      lib.foldl' lib.recursiveUpdate {} (
+        map
+        (file: let
+          safeFile = builtins.unsafeDiscardStringContext file;
+          safeFilePath = /. + safeFile;
+          parts = lib.splitString "/" (lib.removePrefix modulesBase safeFile);
+          offset = if lib.elemAt parts (lib.length parts - 1) == "default.nix" then 2 else 1;
+          modulePath = lib.sublist 1 (lib.length parts - offset - 1) parts;
+          fileName = lib.elemAt parts (lib.length parts - offset);
+          sanitizedName = lib.strings.removeSuffix ".nix" fileName;
+          importedModule = import safeFilePath;
+        in
+          lib.setAttrByPath (modulePath ++ [sanitizedName]) (instantiateModule importedModule))
+        filteredFiles
+      );
 
-    # Create an attribute set where the key is the base name (without .nix)
-    # and the value is the imported Nix file
-    importedModules = lib.foldl' lib.recursiveUpdate {} (
-      map
-      (file: let
-        # Call `unsafeDiscardStringContext` to fix weird dependency issue
-        # (because of `lib.filesystem.listFilesRecursive`)
-        # "a/b/c.nix" || "a/b/c/default.nix"
-        safeFile = builtins.unsafeDiscardStringContext file;
-        safeFilePath = /. + safeFile;
-        # ["a" "b" "c.nix"] || ["a" "b" "c" "default.nix"]
-        parts = (lib.splitString "/" (lib.removePrefix modules-base safeFile));
-        # 1 || 2
-        offset = if lib.elemAt parts (lib.length parts - 1) == "default.nix" then 2 else 1;
-        # ["a" "b"] || ["a" "b"]
-        modulePath = lib.sublist 1 (lib.length parts - offset - 1) parts;
-        # "c.nix" || "c"
-        fileName = lib.elemAt parts (lib.length parts - offset);
-        # "c" || "c"
-        sanitizedName = lib.strings.removeSuffix ".nix" fileName;
-        importedModule = import safeFilePath;
-      in lib.setAttrByPath (modulePath ++ [sanitizedName]) ({ pkgs, ... }: {
-        imports = [(
-          # NOTE: check if module is requesting us to provide it the instanciated
-          #       `tixpkgs`-provided set of packages
-          if builtins.functionArgs importedModule == { tixpkgs = false; }
-          # { tixpkgs }: { pkgs, config, ...}: { ... }
-          then importedModule { tixpkgs = inputs.self.packages.${pkgs.stdenv.hostPlatform.system}; }
-          # { pkgs, config, ...}: { ... }
-          else importedModule
-        )];
-      }))
-      filteredFiles
-    );
+    nixosModulesBase = "${inputs.self.outPath}/modules/nixos";
+    homeManagerModulesBase = "${inputs.self.outPath}/modules/home-manager";
   in {
-    nixosModules' = importedModules;
+    nixosModules' = mkImportedModules nixosModulesBase;
+    homeManagerModules' = mkImportedModules homeManagerModulesBase;
   };
 }
