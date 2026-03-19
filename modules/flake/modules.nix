bd4eebbd5e0357a711596a6cf5bb59298d2bed6b
74k1 <git.t@betsumei.com>
2026-03-19 14:30:09 +0100

    feat(flake): module magic
    Co-authored-by: reo101 <pavel.atanasov2001@gmail.com>

--- modules/flake/modules.nix
+++ modules/flake/modules.nix
@@ -1,50 +1,72 @@
 {
   inputs,
   lib,
+  config,
   ...
-}: {
-  flake = let
-    mkImportedModules = modulesBase:
-      let
-        nixFiles = lib.filesystem.listFilesRecursive modulesBase;
+}:
+{
+  flake =
+    let
+      mkImportedModules =
+        modulesBase:
+        let
+          nixFiles = lib.filesystem.listFilesRecursive modulesBase;
 
-        filteredFiles = builtins.filter (file: lib.strings.hasSuffix ".nix" file) nixFiles;
+          filteredFiles = builtins.filter (file: lib.strings.hasSuffix ".nix" file) nixFiles;
 
-        instantiateModule = importedModule: {pkgs, ...}: let
-          functionArgs = builtins.functionArgs importedModule;
-          extraArgs =
-            lib.optionalAttrs (builtins.hasAttr "inputs" functionArgs) {
-              inherit inputs;
-            }
-            // lib.optionalAttrs (builtins.hasAttr "tixpkgs" functionArgs) {
-              tixpkgs = inputs.self.packages.${pkgs.stdenv.hostPlatform.system};
+          instantiateModule =
+            importedModule:
+            { pkgs, ... }:
+            let
+              functionArgs = builtins.functionArgs importedModule;
+              extraArgs =
+                lib.optionalAttrs (builtins.hasAttr "inputs" functionArgs) {
+                  inherit inputs;
+                }
+                // lib.optionalAttrs (builtins.hasAttr "tixpkgs" functionArgs) {
+                  tixpkgs = inputs.self.packages.${pkgs.stdenv.hostPlatform.system};
+                };
+            in
+            {
+              imports = [
+                (if extraArgs != { } then importedModule extraArgs else importedModule)
+              ];
             };
-        in {
-          imports = [
-            (if extraArgs != { } then importedModule extraArgs else importedModule)
-          ];
-        };
-      in
-      lib.foldl' lib.recursiveUpdate {} (
-        map
-        (file: let
-          safeFile = builtins.unsafeDiscardStringContext file;
-          safeFilePath = /. + safeFile;
-          parts = lib.splitString "/" (lib.removePrefix modulesBase safeFile);
-          offset = if lib.elemAt parts (lib.length parts - 1) == "default.nix" then 2 else 1;
-          modulePath = lib.sublist 1 (lib.length parts - offset - 1) parts;
-          fileName = lib.elemAt parts (lib.length parts - offset);
-          sanitizedName = lib.strings.removeSuffix ".nix" fileName;
-          importedModule = import safeFilePath;
         in
-          lib.setAttrByPath (modulePath ++ [sanitizedName]) (instantiateModule importedModule))
-        filteredFiles
-      );
+        lib.foldl' lib.recursiveUpdate { } (
+          map (
+            file:
+            let
+              safeFile = builtins.unsafeDiscardStringContext file;
+              safeFilePath = /. + safeFile;
+              parts = lib.splitString "/" (lib.removePrefix modulesBase safeFile);
+              offset = if lib.elemAt parts (lib.length parts - 1) == "default.nix" then 2 else 1;
+              modulePath = lib.sublist 1 (lib.length parts - offset - 1) parts;
+              fileName = lib.elemAt parts (lib.length parts - offset);
+              sanitizedName = lib.strings.removeSuffix ".nix" fileName;
+              importedModule = import safeFilePath;
+            in
+            lib.setAttrByPath (modulePath ++ [ sanitizedName ]) (instantiateModule importedModule)
+          ) filteredFiles
+        );
 
-    nixosModulesBase = "${inputs.self.outPath}/modules/nixos";
-    homeManagerModulesBase = "${inputs.self.outPath}/modules/home-manager";
-  in {
-    nixosModules' = mkImportedModules nixosModulesBase;
-    homeManagerModules' = mkImportedModules homeManagerModulesBase;
-  };
+      # { services = { a = ...; b = ...;  }; }
+      # ->
+      # { "services/a" = ...; "services/b" = ...; }
+      flattenModules' =
+        modules':
+        lib.concatMapAttrs (
+          type: modules: lib.mapAttrs' (name: module: lib.nameValuePair "${type}/${name}" module) modules
+        ) modules';
+
+      nixosModulesBase = "${inputs.self.outPath}/modules/nixos";
+      homeManagerModulesBase = "${inputs.self.outPath}/modules/home-manager";
+    in
+    {
+      nixosModules' = mkImportedModules nixosModulesBase;
+      homeManagerModules' = mkImportedModules homeManagerModulesBase;
+
+      nixosModules = flattenModules' config.flake.nixosModules';
+      homeManagerModules = flattenModules' config.flake.homeManagerModules';
+    };
 }
