1a9adfbd968a7d7fa08c59d496c67007d35e0767
74k2 <git.74k2.t@betsumei.com>
2026-06-23 00:15:07 +0200

    feat(fourget): unstable-2026-06-14 -> unstable-2026-06-21
    Update `fourget` from `unstable-2026-06-14` to `unstable-2026-06-21`.
    
    **Built on platform:**
    
    - [x] `x86_64-linux`
    
    Generated by [bumpkin](https://github.com/74k1/bumpkin).

--- pkgs/fo/fourget/default.nix
+++ pkgs/fo/fourget/default.nix
@@ -6,12 +6,12 @@
 
 stdenvNoCC.mkDerivation {
   pname = "4get";
-  version = "unstable-2026-06-14";
+  version = "unstable-2026-06-21";
 
   src = fetchgit {
     url = "https://git.lolcat.ca/lolcat/4get.git";
-    rev = "5a7cecef11d4b728ba202e1a80f5a77b7aee88fe";
-    hash = "sha256-QnzNE3zNVKs0+JggnhNymRmZIUcyKbUGXtj0L5CY7s0=";
+    rev = "347300cc26390917cbd3a0c1a1c8954e2e305ae6";
+    hash = "sha256-bh9yv/o8piyaTJCQvFnIiDMixMhsBb2rNhMrGYOB3To=";
   };
 
   installPhase = ''
